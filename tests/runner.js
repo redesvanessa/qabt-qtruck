617b037ec5a904a149a88b613a0311361ffdff16
Vanessa Redes <redesvanessa@gmail.com>
2022-10-09 22:14:03 -0300

    subindo alteracoes finais

--- tests/runner.js
+++ tests/runner.js
@@ -3,7 +3,8 @@ const tesults = require('cypress-tesults-reporter');
 const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImU1MzgwNDdlLTM4ZjQtNDRiZi04ODBiLTFhN2M2Y2NjYWYxNC0xNjY1MzQ4NjM3ODc1IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiOGY2ZGIxZGYtMTZkOC00YmEwLWE1MDgtNDRjOGM5OWRkYmFlIiwidHlwZSI6InQifQ.QNJIcDKbYRjNvR70EZwkc0h7YX0ULD_aHmFIEkQwH-s'
 
 cypress.run({
-  // specs to run here
+  browser: 'chrome',
+  baseUrl: 'http://localhost:3000'
 })
 .then((results) => {
   const args = {
